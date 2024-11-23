e9020fc3ef44b50efddaf920acef657607f42f9c
spinoandraptos <spinoandraptos@Ubuntu-22.04.myguest.virtualbox.org>
2024-11-22 17:03:18 -0700

    Updates to Lcd test

--- LCD/lcd_test.py
+++ LCD/lcd_test.py
@@ -6,12 +6,14 @@ except RuntimeError:
    
 # Write to LCD in 8-bit data mode
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23],
-              numbering_mode=GPIO.BCM)
+              numbering_mode=GPIO.BOARD)
 
 # Set cursor position to the top row and print "DUMMY"
+print("printing: Dummy")
 lcd.cursor_pos = (0, 3) 
 lcd.write_string(u'DUMMY:')
 
 # Set cursor position to the bottom row and print "Dummy Value"
+print("printing: Dummy value")
 lcd.cursor_pos = (1, 3) 
 lcd.write_string(u'Score: Dummy Value')
