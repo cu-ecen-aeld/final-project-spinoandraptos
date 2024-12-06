52d5c8850dbe960034875be6a29a879b35763886
spinoandraptos <spinoandraptos@Ubuntu-22.04.myguest.virtualbox.org>
2024-12-06 15:44:56 -0700

    Updated pyaline_test

--- PYALINE/pyaline_test.py
+++ PYALINE/pyaline_test.py
@@ -1,9 +1,7 @@
-try:
-	import cmudict
-except RuntimeError:
-	print("Error importing cmudict!")
-	
 from pyaline import lookup_phonemes_score
+import re
+from collections import defaultdict
+from itertools import product as iterprod
 
 def recurse_find_phoneme(s, arpabet):
 	if s in arpabet:
@@ -12,8 +10,8 @@ def recurse_find_phoneme(s, arpabet):
 	partition = sorted(list(range(len(s))), key=lambda x: (x-middle)**2-x)
 	for i in partition:
 		pre, suf = (s[:i], s[i:])
-		if pre in arpabet and recurse_find_phoneme(suf) is not None:
-			return [x+y for x,y in iterprod(arpabet[pre], recurse_find_phoneme(suf))]
+		if pre in arpabet and recurse_find_phoneme(suf, arpabet) is not None:
+			return [x+y for x,y in iterprod(arpabet[pre], recurse_find_phoneme(suf, arpabet))]
 
 def grade_phonemes(transcription, arpabet, reference_word):
 	total_phoneme_score = 0
@@ -33,13 +31,28 @@ def grade_phonemes(transcription, arpabet, reference_word):
 				user_phoneme = ''.join([i for i in user_phoneme if not i.isdigit()])
 				distance = lookup_phonemes_score(user_phoneme, ref_phoneme)
 				total_phoneme_score += distance
-				total_phoneme_score = total_phoneme_score/len(ref_phonemes[0]) * 100
+				total_phoneme_score = total_phoneme_score/len(ref_phonemes[0]) 
 
-	return total_phoneme_score
+	return total_phoneme_score * 100
+	
+dict = []
+comment_string="#"	
+with open('cmudict.dict', 'r') as f:
+    parts = []
+    for line in f:
+        if comment_string:
+            parts = line.strip().split(comment_string)[0].split()
+        else:
+            parts = line.strip().split()
+        thing = re.sub(r"\(\d+\)$", "", parts[0])
+        dict.append((thing, parts[1:]))
+
+cmudict = defaultdict(list)
+for key, value in dict:
+    cmudict[key].append(value)
 
 reference_word = "library"
-arpabet = cmudict.dict()
-transcription = [["leebwary"]]
-score = grade_phonemes(transcription, arpabet, reference_word)
+transcription = "leebwary"
+score = grade_phonemes(transcription, cmudict, reference_word)
 print(score)
 	
