dfdac27d174d9762d85cd74359a370d04cc0b7fc
spinoandraptos <spinoandraptos@Ubuntu-22.04.myguest.virtualbox.org>
2024-12-08 11:08:13 -0700

    Updated integrated_test

--- integration/gpio_lcd_mic_asr_test.py
+++ integration/gpio_lcd_mic_asr_test.py
@@ -38,7 +38,7 @@ PUSH_BUTTON = 11
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(True)
-GPIO.setup(PUSH_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PUSH_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Write to LCD in 8-bit data mode
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23],
